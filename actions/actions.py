28e593ce0f830660143ffdda84235cf7767b647a
ArthurCoget <arthur.coget@student.ucll.be>
2023-11-10 00:35:55 +0100

    Final push for shell

--- actions/actions.py
+++ actions/actions.py
@@ -11,7 +11,9 @@ from rasa_sdk.types import DomainDict
 import requests
 
 TIME_ZONE_URL = "http://worldtimeapi.org/api/timezone/"
-
+LOC_INF_URL = "https://en.wikivoyage.org/w/api.php"
+OVER_POI_URL = "https://overpass-api.de/api/interpreter"
+GEO_INF_URL = "http://api.openweathermap.org/geo/1.0/direct"
 class ActionLookupFlights(Action):
 
     def getAirports(self,city):
@@ -81,8 +83,6 @@ class ActionLookupFlights(Action):
     def name(self) -> Text:
         return "action_get_flights"
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        
-        print("Hello?")
 
         start_place = tracker.get_slot("start_place")
         goto_place = tracker.get_slot("goto_place")
@@ -153,3 +153,120 @@ class ActionShowTimeZone(Action):
             output = 'Ops! There are too many requests on the time zone API. Please try a few moments later...'
         dispatcher.utter_message(text=output)       
         return []
+
+class ActionGetInformation(Action):
+    def name(self):
+        return "action_get_information"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain):
+        location = tracker.get_slot("city_name")
+        print(location)
+
+        if location: 
+            params = {
+                "action": "query",
+                "titles": location,
+                "prop": "extracts",
+                "exintro": True,
+                "format": "json",
+            }
+            response = requests.get(LOC_INF_URL, params=params)
+
+            data = response.json()
+            page_data = data.get("query", {}).get("pages", {})
+
+            if page_data:
+                page = next(iter(page_data.values()))
+                extract = page.get("extract")
+                
+                if extract:
+                    parser = MyHTMLParser()
+                    parser.feed(extract)
+                    response_text = f"Here is some information about {location}: {parser.data}"
+                else:
+                    response_text = f"I couldn't find information of {location}."
+            else:
+                response_text = f"I couldn't find information of {location}."
+
+        else:
+            response_text = "Please provide a valid location."
+        dispatcher.utter_message(response_text)
+        return []
+    
+class ActionGetPointOfInterest(Action):
+    def get_geolocation(self, city):
+        openweathermap_api_key = "8105293046c7f3f036461a31be0278bd"
+
+        params = {
+                    "q": city,
+                    "appid": openweathermap_api_key
+                }
+        response = requests.get(GEO_INF_URL, params=params)
+
+        data = response.json()
+        firstResult = data[0]
+
+        if "lat" in firstResult and "lon" in firstResult:
+            lat = firstResult["lat"]
+            lon = firstResult["lon"]
+            return lat, lon
+        else:
+            return None
+    
+    def name(self) -> Text:
+        return "action_get_pois"
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        city = tracker.get_slot("local_act")
+        print(city)
+        if city:
+            lat, lon = self.get_geolocation(city)
+            if lat is not None and lon is not None:
+                query = f"""
+                    [out:json];
+                    (
+                    node({lat - 0.5}, {lon - 0.5}, {lat + 0.5}, {lon + 0.5})["tourism"];
+                    );
+                    out body 10;
+                    """
+
+                response = requests.post(OVER_POI_URL, data=query)
+                data = response.json()
+
+
+                tourist_pois = [
+                    item for item in data['elements'] if 'tourism' in item['tags']
+                ]
+                if tourist_pois:
+                    response_text = f"Here some points off interest nearby {city}.\n"
+                    for poi in tourist_pois:
+                        name = poi.get('tags', {}).get('name', 'N/A')
+                        category = poi.get('tags', {}).get('tourism', 'N/A')
+                        website = poi.get('tags', {}).get('website', 'N/A')
+                        info = poi.get('tags', {}).get('information', 'N/A')
+                        artworktype = poi.get('tags', {}).get('artwork_type', 'N/A')
+                        if name != 'N/A':
+                            response_text += f"You have {name}, category: {category}.\n"
+                        elif info != 'N/A':
+                            response_text += f"You have {info}, category: {category}.\n"
+                        elif artworktype != 'N/A':
+                            response_text += f"You have {artworktype}, category: {category}.\n" 
+                        if website != 'N/A':
+                            response_text += f"Website: {website}\n"
+                else:
+                    response_text = "No points of interest found in the specified city using OpenStreetMap."
+            else:
+                response_text = "Did not find the given city."
+        else:
+            response_text = "Please provide a valid city name."
+
+        dispatcher.utter_message(response_text)
+        return []
+
+
+class MyHTMLParser(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.data = ""
+    
+    def handle_data(self, data):
+        self.data += data
